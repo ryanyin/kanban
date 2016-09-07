df4e91fa3410e3c964a643d74207332d395e6848
尹勇 <yinyong@it.com>
2016-09-07 19:27:40 +0800

    配置热加载

---
+++ app/component.js
@@ -0,0 +1,9 @@
+/**
+ * Created by z673820 on 2016/9/7.
+ */
+
+module.exports=function () {
+    var element=document.createElement('h1');
+    element.innerHTML='hello my world im new a test haha why the ide is wrong';
+    return element;
+};
