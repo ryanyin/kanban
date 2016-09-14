d75ec5ac298afa6055b39fdec30fc1119b7198c3
尹勇 <yinyong@it.com>
2016-09-14 17:20:59 +0800

    便签增删改

--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,6 @@
  * Created by z673820 on 2016/9/7.
  */
 
-/*require('./main.css');
-var component=require('./component');
-var app=document.createElement('div');
-
-document.body.appendChild(app);
-
-app.appendChild(component());*/
 
 import './main.css';
 
